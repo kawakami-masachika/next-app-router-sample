59dc1dd9ce99bb46bdb2f8968ab009c48def590a
川上眞周 <kawakami.masachika@kawakamishinshuunoMacBook-Pro.local>
2024-12-08 11:56:32 +0900

    疎通確認

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,8 @@
 import Image from "next/image";
 
 export default async function Home() {
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon-species/150")
+  const response = await fetch("http://localhost:3000//health_check")
   const data = await response.json();
-  console.log(data.color.name)
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -15,7 +14,7 @@ export default async function Home() {
           height={38}
           priority
         />
-        {data.color.name}
+        {data.message}
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
             Get started by editing{" "}
